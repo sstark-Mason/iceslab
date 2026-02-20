e8ab7a656648513da348314492bbc88c7dfb6c74
sven <sstark-Mason@gmail.com>
2026-02-20 16:18:26 -0500

    dev update

--- assets/scripts/install_upgrade_packages.sh
+++ assets/scripts/install_upgrade_packages.sh
@@ -64,3 +64,7 @@ dnf install -y openh264 gstreamer1-plugin-openh264 mozilla-openh264
 dnf install -y cabextract xorg-x11-font-utils fontconfig
 rpm -i --nodigest --nosignature https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 fc-cache -fv
+
+touch .post_install
+
+reboot
